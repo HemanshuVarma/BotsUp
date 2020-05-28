b57ff6e36e67a0ab5ce3a1f0ccb4919caed5a302
HemanshuVarma <hemanshuvarma916@gmail.com>
2020-05-28 23:02:17 +0530

    Add DataBinding and NavigationGraph

--- app/src/main/java/com/varma/hemanshu/botsup/MainActivity.kt
+++ app/src/main/java/com/varma/hemanshu/botsup/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.varma.hemanshu.botsup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.varma.hemanshu.botsup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
     }
 }
---
+++ app/src/main/java/com/varma/hemanshu/botsup/fragments/ChatFragment.kt
@@ -0,0 +1,23 @@
+package com.varma.hemanshu.botsup.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.varma.hemanshu.botsup.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+}
