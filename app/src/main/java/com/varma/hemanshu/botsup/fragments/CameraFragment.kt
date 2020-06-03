e8502b4b8cf82ceeae7142b93763b61f700a2804
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-06-03 22:08:09 +0530

    Fix Play/Pause Animation logic

--- app/src/main/java/com/varma/hemanshu/botsup/fragments/CameraFragment.kt
+++ app/src/main/java/com/varma/hemanshu/botsup/fragments/CameraFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.varma.hemanshu.botsup.R
+import kotlinx.android.synthetic.main.fragment_camera.*
+import timber.log.Timber
 
 /**
  *  Fragment for Camera Section
@@ -18,6 +20,22 @@ class CameraFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //Playing animation only when the fragment is active
+        coming_soon_anim.playAnimation()
+        Timber.i("Animation Resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        //Stopping animation only when the fragment is inactive
+        coming_soon_anim.pauseAnimation()
+        Timber.i("Animation Paused")
     }
 
     //Instance used by ViewPager to inflate this Fragment
